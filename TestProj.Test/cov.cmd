2c3c0e3a048c14cd7b5f814f41cf1d6f54fd2a11
dgwynnjones <d.gwynn-jones@screenaustralia.com.au>
2018-10-02 14:58:38 +1000

    opencover filtered down to TestProj.Business.*

--- TestProj.Test/cov.cmd
+++ TestProj.Test/cov.cmd
@@ -4,12 +4,7 @@ rem @echo off
 cls
 cls
 
-REM  "..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" -filter:"+[TestProj.Business.ClassUnderTest.*]* -[TestProj.Test.*]*" -targetargs:"TestProj.Test.dll " -output:"_CodeCoverageResult.xml" -register:user
-"..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -filter:"+[TestProj*]*" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe"  -targetargs:"TestProj.Test.dll " -output:"_CodeCoverageResult.xml" -register:user
-
-
-rem -filter:"+[ProjToTest]ProjToTest*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml"
-rem --where:"cat != blah"
+"..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -filter:"+[TestProj*]* -[TestProj.Test]*" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe"  -targetargs:"TestProj.Test.dll " -output:"_CodeCoverageResult.xml" -register:user
 
 ECHO .
 ECHO .

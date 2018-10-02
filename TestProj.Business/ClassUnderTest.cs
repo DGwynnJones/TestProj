93d0bae0af29e0605f638ae220bb485b29dd9e2f
dgwynnjones <d.gwynn-jones@screenaustralia.com.au>
2018-10-02 14:15:35 +1000

    nunit working

---
+++ TestProj.Business/ClassUnderTest.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProj.Business
+{
+    public class ClassUnderTest
+    {
+
+        public static string GetAnswer(string name)
+        {
+            return "Hello " + name;
+        }
+
+    }
+}
---
+++ TestProj.Test/BasicTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace TestProj.Test
+{
+    [TestFixture]
+    public class BasicTests
+    {
+
+        [Test]
+        public void Basic_1()
+        {
+            var result = TestProj.Business.ClassUnderTest.GetAnswer("Bob");
+
+            Assert.That(result, Is.EqualTo("Hello Bob"));
+
+        }
+
+    }
+}

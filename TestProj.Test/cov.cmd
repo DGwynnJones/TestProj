70a30f05ae47879e02b49cb4f3e32438debda954
dgwynnjones <d.gwynn-jones@screenaustralia.com.au>
2018-10-02 14:56:55 +1000

    opencover almost working

--- TestProj.Test/cov.cmd
+++ TestProj.Test/cov.cmd
@@ -1,17 +1,22 @@
-﻿@echo off
-@echo off
+﻿rem @echo off
+rem @echo off
 
 cls
 cls
 
-
-REM "..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" -filter:"+[CommBankStatementPDF.Business]CommBankStatementPDF.Business*" -targetargs:"CommBankStatementPDF.Tests.dll --where:"cat != IntegrationTests"" -output:".\CodeCoverage\_CodeCoverageResult.xml"
-
-"..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" -filter:"+[ProjectChabrier.Business]ProjectChabrier.Business.* +[ProjectChabrier.Web]ProjectChabrier.Web.ApplicationModels.* +[ProjectChabrier.Web]ProjectChabrier.Web.Controllers.*" -targetargs:"ProjectChabrier.Test.dll " -output:"_CodeCoverageResult.xml" -register:user
+REM  "..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" -filter:"+[TestProj.Business.ClassUnderTest.*]* -[TestProj.Test.*]*" -targetargs:"TestProj.Test.dll " -output:"_CodeCoverageResult.xml" -register:user
+"..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -filter:"+[TestProj*]*" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe"  -targetargs:"TestProj.Test.dll " -output:"_CodeCoverageResult.xml" -register:user
 
 
 rem -filter:"+[ProjToTest]ProjToTest*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml"
 rem --where:"cat != blah"
+
+ECHO .
+ECHO .
+ECHO Generating report
+ECHO .
+ECHO .
+
 
 "..\..\..\packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe" "-reports:_CodeCoverageResult.xml" "-targetdir:Report"
 

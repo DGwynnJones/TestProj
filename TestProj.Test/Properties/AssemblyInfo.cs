c337c4b890247a0ca50ab9e730c7e3ae33efbc17
dgwynnjones <d.gwynn-jones@screenaustralia.com.au>
2018-10-02 14:03:44 +1000

    initial

--- TestProj.Test/Properties/AssemblyInfo.cs
+++ TestProj.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("TestProj.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("TestProj.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ba5450a7-84ae-4351-a6cf-df1865f585c0")]
+[assembly: Guid("d6d299d8-bd11-4050-83ce-076a991f2579")]
 
 // Version information for an assembly consists of the following four values:
 //

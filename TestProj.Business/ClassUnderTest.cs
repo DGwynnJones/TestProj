a8e5c0ac4bf587561173a6e2ae63ee48e8c88d89
dgwynnjones <d.gwynn-jones@screenaustralia.com.au>
2018-10-02 15:04:24 +1000

    opencover

--- TestProj.Business/ClassUnderTest.cs
+++ TestProj.Business/ClassUnderTest.cs
@@ -3,6 +3,11 @@
     public class ClassUnderTest
     {
 
+        public static string SatHello(string name)
+        {
+            return "Hello " + name;
+        }
+
         public static string GetAnswer(string name)
         {
             return "Hello " + name;

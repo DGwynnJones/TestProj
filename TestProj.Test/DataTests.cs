208bccb3dc89415118a861d65d7e26138bccd7e2
dgwynnjones <David.Gwynn-Jones@screenaustralia.gov.au>
2018-10-08 10:19:46 +1100

    Added Avails DB entity model

---
+++ TestProj.Test/DataTests.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestProj.Data;
+
+namespace TestProj.Test
+{
+    [TestFixture]
+    public class DataTests
+    {
+        [Test]
+        public void Basic_1()
+        {
+            using (var context = new TestDBEntities())
+            {
+                var t = new Person
+                {
+                    FirstName = "Bob",
+                    LastName="Roberts"
+                };
+                context.Person.Add(t);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}

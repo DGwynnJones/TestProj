5d92ab83d585827171d2eed2844f8b7929ab80ed
dgwynnjones <David.Gwynn-Jones@screenaustralia.gov.au>
2018-11-21 09:19:28 +1100

    T4 cleanup

--- TestProj.Test/TextTemplate1.cmd
+++ TestProj.Test/TextTemplate1.cmd
@@ -4,13 +4,10 @@ CLS
 
 
 ECHO RG: C:\Users\dgwynnjones\source\repos\TestProj
-ECHO RG: 1
-ECHO RG: C:\Users\dgwynnjones\source\repos\TestProj\packages\ReportGenerator.4.0.4\tools\net47\ReportGenerator.exe
-ECHO Script generated at: 21/11/2018 9:07:24 AM
+ECHO Script generated at: 21/11/2018 9:19:09 AM
 
-"C:\Users\dgwynnjones\source\repos\TestProj\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -filter:"+[TestProj*]* -[TestProj.Test]*" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe"  -targetargs:"TestProj.Test.dll " -output:"_CodeCoverageResult.xml" -register:user
+"C:\Users\dgwynnjones\source\repos\TestProj\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -filter:"+[TestProj*]* -[TestProj.Test]*" -target:"C:\Users\dgwynnjones\source\repos\TestProj\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" -targetargs:"TestProj.Test.dll" -output:"_CodeCoverageResult.xml" -register:user
 
 "C:\Users\dgwynnjones\source\repos\TestProj\packages\ReportGenerator.4.0.4\tools\net47\ReportGenerator.exe" "-reports:_CodeCoverageResult.xml" "-targetdir:Report"
 
 Report\index.htm
-
